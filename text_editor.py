58620e27be8e951b9df2f3fdcc8ef89ffea27c38
Francis Rouhani <frouhani556@outlook.com>
2025-09-07 12:52:26 -0700

    feat: added menu bar, and ability to open files

--- text_editor.py
+++ text_editor.py
@@ -2,7 +2,8 @@ from tkinter import *
 import tkinter.filedialog
 
 # Main Window
-main = Tk("Text Editor")
+main = Tk()
+main.title("Text Editor")
 text = Text(main)
 text.grid()
 
@@ -10,39 +11,64 @@ text.grid()
 def saveas():
     global text
     t = text.get("1.0", "end-1c")
-    savelocation = tkinter.filedialog.asksaveasfilename(defaultextension=".txt",
-                                                        filetypes=[("Text files", "*.txt"),
-                                                                   ("All files", "*.*")])
+    savelocation = tkinter.filedialog.asksaveasfilename(
+        defaultextension=".txt",
+        filetypes=[("Text files", "*.txt"),("All files", "*.*")])
     # Takes what the user writes and saves it as a file in the location they choose
     file1 = open(savelocation, "w+")
     file1.write(t)
     file1.close()
 
-# Button used for save text function
-button = Button(main, text="Save", command=saveas)
-button.grid()
+# Function for opening a file (currently only txt)
+def open_file():
+    # Asks user to select a file
+    filepath = tkinter.filedialog.askopenfilename(
+        filetypes=[("Text files", "*.txt"), ("All files", "*.*")]
+    )
+
+    # Only activates if the user selects a file
+    if filepath:
+        with open(filepath, "r") as file:
+            content = file.read()
+
+        # Clear current text
+        text.delete("1.0", "end-1c")
+        # Insert file text
+        text.insert("1.0", content)
 
 # Fonts
-def FontHelvetica():
+def FontVerdana():
     global text
-    text.config(font="Helvetica")
+    text.config(font=("Verdana", 12))
 
 def FontCourier():
     global text
-    text.config(font="Courier")
+    text.config(font=("Courier", 12))
 
-# Creates font selector widget
-font = Menubutton(main, text="Font")
-font.grid()
-font.menu=Menu(font, tearoff=0) # creates drop down menu
-font["menu"] = font.menu
-# Keeps track of whether an item is checked
-helvetica = IntVar()
-courier = IntVar()
-# Adds two buttons to the font menu
-font.menu.add_checkbutton(label="Helvetica", variable=helvetica,
-command = FontHelvetica)
-font.menu.add_checkbutton(label="Courier", variable=courier,
-command=FontCourier)
+def FontArial():
+    global text
+    text.config(font=("Arial", 12))
+
+def FontComicSans():
+    global text
+    text.config(font=("Comic Sans MS", 12))
+
+# Creates a top menu bar
+menubar = Menu(main)
+main.config(menu=menubar)
+
+# File Menu Button
+file_menu = Menu(menubar, tearoff=0)
+file_menu.add_command(label="Save as...", command=saveas)
+file_menu.add_command(label="Open", command=open_file)
+menubar.add_cascade(label="File", menu=file_menu)
+
+# Font menu Button
+font_menu = Menu(menubar, tearoff=0)
+font_menu.add_radiobutton(label="Verdana", command=FontVerdana)
+font_menu.add_radiobutton(label="Courier", command=FontCourier)
+font_menu.add_radiobutton(label="Arial", command=FontArial)
+font_menu.add_radiobutton(label="Comic Sans", command=FontComicSans)
+menubar.add_cascade(label="Font", menu=font_menu)
 
 main.mainloop()
